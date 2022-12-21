025ba1c459260de206a3c26b1aa52ec7b8a767a5
eduardot23 <eduardojeda.t23@gmail.com>
2022-12-21 13:36:24 -0600

    Solucion de errores, callback hell y obtener info de una api

--- src/challenge.js
+++ src/challenge.js
@@ -1,12 +1,13 @@
-const XMLHttpRequest = require("xmlhtttprequest").XMLHttpRequest
-const API = "https://api.scuelajs.co/api/v1"
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+const API = "https://api.escuelajs.co/api/v1"
 
 function fetchData(urlAPI, callback){
         let xhttp = new XMLHttpRequest()
 
         xhttp.open("GET", urlAPI, true)
         xhttp.onreadystatechange = function (event){
-            if(xhttp.readystate == 4){
+            if(xhttp.readyState == 4){
+                
                 if(xhttp.status == 200){
                     callback(null, JSON.parse(xhttp.responseText))
                 }else{
@@ -15,15 +16,21 @@ function fetchData(urlAPI, callback){
                 }
             }
         }
-    xhttp.send()
+        xhttp.send()   
 } 
 
 fetchData(`${API}/products`, function(error1, data1){
-    if(error1) {return console.error(error1)}
-    fetchData(`${API}/products${data1[0].id}`, function (error2, data2){
-        if(error2) {return console.error(error2)}
+    if(error1) {
+    return console.error(error1)
+    }
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
+        if(error2) {
+        return console.error(error2)
+        }
         fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3){
-            if(error3) {return console.error(error3)}
+            if(error3) {
+            return console.error(error3)
+            }
             console.log(data1[0])
             console.log(data2.title)
             console.log(data3.name)
